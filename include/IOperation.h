3a938d66ddfa10eaea3f32db0f09dff0fbebf9f9
ZLinuxFan <ZLinuxFan@gmail.com>
2016-04-13 17:54:48 +0300

    Added interface.

---
+++ include/IOperation.h
@@ -0,0 +1,36 @@
+//
+// Created by kde on 4/13/16.
+//
+
+#ifndef CPP_CALCULATOR_IOPERATION_H
+#define CPP_CALCULATOR_IOPERATION_H
+
+// Interface
+class IOperation {
+public:
+  /**
+     * Return result of the evaluation of the current operation and all underlying operations.
+     *
+     * @return Value of this operation
+     */
+  virtual double evaluate() = 0;
+
+  /**
+     * Converts current operation to string representation
+     *
+     * @return String representation of current operation like "1+4".
+     */
+  virtual std::string toString();
+
+  /**
+     * Return a number of nested operations for current operation.
+     *
+     * @return Value of this operation
+     */
+  virtual int getDepth();
+
+  virtual ~IOperation();
+};
+
+
+#endif //CPP_CALCULATOR_IOPERATION_H
