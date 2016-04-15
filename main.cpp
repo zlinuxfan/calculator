ede7039cdd6d2440838fea61de614b5315d0f722
ZLinuxFan <ZLnuxFan@gmail.com>
2016-04-15 14:09:26 +0300

    added ptr

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "Sum.h"
+#include "Numbers.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+  IOperation * firstArgument = new Numbers(7),
+             * secondArgument = new Numbers(3);
+
+    Sum * sum = new Sum(firstArgument, secondArgument);
+
+    sum->toString();
+  std::cout << sum->firstArgument() << "\n";
+
     return 0;
 }
